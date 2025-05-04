4aa815c4131368bb30f8b5f9009b2abe47c8ce60
saurluca <spuckteller@gmail.com>
2025-05-04 17:43:59 +0200

    added bias

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import torch
 
 
-def manual(X_value, neuron_1, neuron_2, neuron_3):
+def manual(X_value, neuron_1, neuron_2, neuron_3, neuron_1_bias, neuron_2_bias):
     
     # layer 1
     print(f"input {X_value}")
-    out_1 = torch.dot(neuron_1, X_value)
+    X_value_1 = torch.cat((X_value, neuron_1_bias))
+    out_1 = torch.dot(neuron_1, X_value_1)
     print(f"output neuron_1 {out_1}")
-    out_2 = torch.dot(neuron_2, X_value)
+    
+    X_value_2 = torch.cat((X_value, neuron_2_bias))
+    out_2 = torch.dot(neuron_2, X_value_2)
     print(f"output neuron_2 {out_2}")
-    print(f"shape of out_2 {out_2.shape}")
+    
     xy_2 = torch.tensor([out_1.item(), out_2.item()])
     print(f"concatinated out_1 and out_1 {xy_2}")
 
@@ -40,16 +43,19 @@ def main():
     # X_value = torch.tensor([1.0, 2.0], requires_grad=False),
     X_value = torch.tensor([1.0, 2.0])
     
-    # layer 1
-    neuron_1 = torch.tensor([2.0, 2.0])
-    neuron_2 = torch.tensor([-2.0, -4.0])
-
+    # layer 1 
+    # third item corresponds to the weighting of the
+    neuron_1 = torch.tensor([2.0, 2.0, 1.0])
+    neuron_1_bias = torch.tensor([1.0])
+    neuron_2 = torch.tensor([-2.0, -4.0, -2.0])
+    neuron_2_bias = torch.tensor([1.0])
+    
     # layer 2
     neuron_3 = torch.tensor([1.0, 1.0])
     
-    manual(X_value, neuron_1, neuron_2, neuron_3)
+    manual(X_value, neuron_1, neuron_2, neuron_3, neuron_1_bias, neuron_2_bias)
     print("\n", 50 * "=", "\n")
-    torch_way(X_value, neuron_1, neuron_2, neuron_3)
+    # torch_way(X_value, neuron_1, neuron_2, neuron_3)
 
 
 if __name__ == "__main__":
